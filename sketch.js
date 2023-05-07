13b47c2c566c1d7ded3952f7cd70e7f5b0b79ec5
jamvaneyck <132843242+jamvaneyck@users.noreply.github.com>
2023-05-07 15:16:33 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,27 @@
-var base;
+let base;
+let top;
+let dress;
 
 function setup() {
-  createCanvas(3900, 3787);
-  base= loadImage('base.png');
+  createCanvas(windowWidth, windowHeight);
 }
 
+function preload() {
+  base= loadImage('base.png');
+  top= loadImage('top.png');
+  dress= loadImage('dress.png');
+}
+
+let value = 0;
 function draw() {
   image(base,0,0);
+  image(dress,0,0);
+  image(top,0,0);
+  if (mouseIsPressed) {
+    top.show();
+    dress.hide();
+  } else {
+    top.hide();
+  }
+
 }
